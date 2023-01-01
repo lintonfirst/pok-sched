8c88bef09185d27f7186883bf7103cb61a60bc06
linton <sczlk@163.com>
2023-01-01 11:19:03 +0800

    task dependency

--- experiments/multitasks/src/main.c
+++ experiments/multitasks/src/main.c
@@ -4,44 +4,33 @@
 #include <core/partition.h>
 #include <types.h>
 
+uint32_t frameCounter=0;
+uint32_t processCounter=0;
+uint32_t detectedNum=0;
+uint32_t sendCounter=0;
 
-static void task_ctrl();
-static void task_net();
-static void task_video();
+static void fetch_video_frame();
+static void unet_segmentation();
+static void send_processed_frame();
 
 int main() {
     uint32_t tid;
     pok_thread_attr_t tattr;
     memset(&tattr, 0, sizeof(pok_thread_attr_t));
 
-    tattr.priority = 40;
-    // tattr.period = 1000;
-    tattr.period = 3000;
-    // tattr.time_capacity = 100;
-    tattr.time_capacity = 300;
-    // tattr.deadline = 1000;
-    tattr.deadline = 500;
-    tattr.entry = task_ctrl;
+    tattr.period = 100000000;
+    tattr.time_capacity = 30;
+    tattr.entry = fetch_video_frame;
     pok_thread_create(&tid, &tattr);
 
-    tattr.priority = 30;
-    // tattr.period = 100;
-    tattr.period = 1000;
-    // tattr.time_capacity = 10;
-    tattr.time_capacity = 200;
-    // tattr.deadline = 20;
-    tattr.deadline = 1000;
-    tattr.entry = task_net;
+    tattr.period = 100000000;
+    tattr.time_capacity = 20;
+    tattr.entry = unet_segmentation;
     pok_thread_create(&tid, &tattr);
 
-    tattr.priority = 10;
-    // tattr.period = 500;
-    tattr.period = 2000;
-    // tattr.time_capacity = 200;
-    tattr.time_capacity = 1000;
-    // tattr.deadline = 300;
-    tattr.deadline = 2000;
-    tattr.entry = task_video;
+    tattr.period = 500000000;
+    tattr.time_capacity = 5;
+    tattr.entry = send_processed_frame;
     pok_thread_create(&tid, &tattr);
 
     pok_partition_set_mode(POK_PARTITION_MODE_NORMAL);
@@ -49,20 +38,40 @@ int main() {
     return 0;
 }
 
-static void task_ctrl() {
-    printf("[APP_4 task_ctrl]: Control!\n");
-    for (;;) {
+static void fetch_video_frame() {
+    while(1){
+        for (int i=0;i<300000;i++) {
+            ;
+        }
+        frameCounter+=1;
+        printf("frameCounter %u,processCounter %u,sendCounter %u\n",frameCounter,processCounter,sendCounter);
     }
 }
 
-static void task_net() {
-    printf("[APP_4 task_net]: Net!\n");
-    for (;;) {
+static void unet_segmentation() {
+    while(1){
+        if(frameCounter>processCounter){
+            for (int i=0;i<200000;i++) {
+                
+            }
+            processCounter+=1;
+            if(processCounter%11==0){
+                detectedNum+=1;
+            }
+            printf("frameCounter %u,processCounter %u,sendCounter %u\n",frameCounter,processCounter,sendCounter);
+        }  
     }
 }
 
-static void task_video() {
-    printf("[APP_4 task_video]: Video!\n");
-    for (;;) {
+static void send_processed_frame() {
+    while(1){
+        if(detectedNum>10){
+            for (int i=0;i<50000;i++) {
+                ;
+            }
+            detectedNum-=10;
+            sendCounter+=1;
+            printf("frameCounter %u,processCounter %u,sendCounter %u\n",frameCounter,processCounter,sendCounter);
+        }
     }
 }
